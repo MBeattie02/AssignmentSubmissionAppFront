a316d340f836d5ce39976dbb026311e2f2286cb9
MBeattie02 <91721899+MBeattie02@users.noreply.github.com>
2022-10-10 09:40:46 +0100

    initial commit

---
+++ src/Homepage/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import logo from "./github.png";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "../NavBar";
+
+const Homepage = () => {
+  return (
+    <div className="nav">
+      <Container>
+        <Row>
+          <Col md>
+            {" "}
+            <img src={logo} alt="logo" className="logo" />{" "}
+          </Col>
+          <Col md>
+            <NavBar />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+export default Homepage;
